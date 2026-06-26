021175d7ca5c4ebd26589faabc6087e14edef003
srilekha-cmyk <bsrilekha75@gmail.com>
2026-06-26 19:25:22 +0530

    Add age check for voting eligibility

---
+++ program1.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
+
+# Output:
+# Enter your age: 20
+# Eligible to vote
